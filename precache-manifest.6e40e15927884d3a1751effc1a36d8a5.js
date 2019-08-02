c726357954b443302edd8ec5d9bc01512901487c
Mariselvan <mariselvan.murugasan@zoomrx.com>
2019-08-03 00:35:19 +0530

    Updates

---
+++ precache-manifest.6e40e15927884d3a1751effc1a36d8a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77cda6989b2561196238f0bef23b798",
+    "url": "/cow-bull-game/index.html"
+  },
+  {
+    "revision": "5db58abd1974e594e21e",
+    "url": "/cow-bull-game/static/css/main.53d4ee6d.chunk.css"
+  },
+  {
+    "revision": "52172a67b570198b2a13",
+    "url": "/cow-bull-game/static/js/2.1114c48f.chunk.js"
+  },
+  {
+    "revision": "5db58abd1974e594e21e",
+    "url": "/cow-bull-game/static/js/main.db23df44.chunk.js"
+  },
+  {
+    "revision": "0e805ff53b23fa04781f",
+    "url": "/cow-bull-game/static/js/runtime~main.ca23038b.js"
+  },
+  {
+    "revision": "96c3ef1a1de59c0b4419a3b845597d96",
+    "url": "/cow-bull-game/static/media/cow.96c3ef1a.png"
+  }
+]);
